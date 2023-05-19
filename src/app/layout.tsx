12e15111e791e3571d6bac868769bc86dfa950b2
Trung4123 <thieuvinhtrung4123@gmail.com>
2023-05-19 19:07:53 +0700

    create Home Page interface
    add:
    + Header Component
    + animation.css  + button.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import './globals.css'
+import './buttons.css'
+import './animation.css'
 import { Inter } from 'next/font/google'
+
+import Header from '@/components/Header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,9 +1,29 @@
+import useHomePage from './useHomePage';
+
+
 function HomePage() {
+  const { handleForwardToLink } = useHomePage();
+
   return (
     <div className="w-screen h-screen overflow-hidden relative">
       <video autoPlay muted loop className="absolute top-0 left-0 right-0">
         <source src="./assets/videos/intro.mp4" type="video/mp4" />
       </video>
+
+      <div className='absolute top-0 left-0 w-full h-full bg-[#000000d5]'></div>
+
+      {/* Slogan */}
+      <div className='w-full absolute bottom-[30%] left-0'>
+        <h1 className='text-white text-center text-5xl font-VarelaRound'>WE PRODUCE GREAT STORY.</h1>
+        <h1 className='text-white text-center text-5xl font-VarelaRound'>WITH GOOD PRICE </h1>
+      </div>
+
+      {/* Button */}
+      <div className='absolute bottom-[15%] left-[50%] -translate-x-1/2'>
+        <button onClick={handleForwardToLink} className='btn_neon neon_red mx-4'>SHOWREEL</button>
+        <button className='btn_neon neon_cyan mx-4'>CONTACT US</button>
+      </div>
+
     </div>
   );
 }
