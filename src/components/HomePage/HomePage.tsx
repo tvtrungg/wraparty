37dd2a73400f0fa2f0d011035967030a46032a8e
nam.vhh <namhoailu@gmail.com>
2023-05-19 17:09:46 +0700

    feat: carousel layout

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className="w-screen h-screen overflow-hidden relative">
+      <video autoPlay muted loop className="absolute top-0 left-0 right-0">
+        <source src="./assets/videos/intro.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default HomePage;
