caff1e62d94ac3894f4366bdbc37aea83b59f651
namhoai <namhoailu@gmail.com>
2023-05-21 10:48:35 +0700

    feat: about page

---
+++ src/app/data.ts
@@ -0,0 +1,11 @@
+export const NUMBER_PAGES = 2;
+
+export const aboutUs = {
+  slogan: "TEAM UP MAKES HITS",
+  info: [
+    "WRAPARTY is a young but professional, full-service production house located in Saigon, Vietnam. We are committed to blowing a fresh way of producing commercials for our industry.",
+    "Currently, we have created partnerships with some of the greatest post-production houses producing best-in-class outcomes.",
+    "We always try to bring in exclusive directors and production crews in every project. Our production facilities also offer fully functional line production services that cater to all projects. By that, we commit to the same quality in each project.",
+    "Here are some significant clients trusting us: Unilever, Nestle, Acecook, Nivea, Maxkleen, Yamaha, KitKat, Panasonic, Aji-No-Moto, Dutch Lady, etc.",
+  ],
+};
